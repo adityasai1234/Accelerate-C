850d2732184fbaa632aa81ac303ee3bdacdd0a83
Aditya <adityasai3230@gmail.com>
2026-01-14 20:52:35 +0530

    idk why i forgot to save my .c file so my old save code was commited, but this the code i meant to commit

--- include/core/linalg.c
+++ include/core/linalg.c
@@ -1,3 +1,14 @@
 #include "core/linalg.h"
 #include <math.h>
 
+// matrix multiplication is done using vDSP_mmul
+// vDSP_mmul uses 'Row-Major' or 'Column-Major' logic depending on stride
+
+void mat_mul(const float* A, const float* B, float* C, int m, int n, int k) {
+   //M: rows of A and C 
+  //N: columns of B and C 
+  //P: colums of A and rows of B
+  vDSP_mmul(A, 1 B, 1 C, 1 m, n, k);
+
+}
+
