06dc98eb22f2c26ebab06fcb2c4c89e067ab8d4e
Aditya <adityasai3230@gmail.com>
2026-01-14 23:12:49 +0530

    added sigmoied function, dot product and implimented hardware accelerated  activation and error functions

--- include/core/linalg.c
+++ include/core/linalg.c
@@ -8,7 +8,20 @@ void mat_mul(const float* A, const float* B, float* C, int m, int n, int k) {
    //M: rows of A and C 
   //N: columns of B and C 
   //P: colums of A and rows of B
-  vDSP_mmul(A, 1 B, 1 C, 1 m, n, k);
+  vDSP_mmul(A, 1, B, 1, C, 1, m, n, k);
+}
 
+void sigmoid_vec(float* data, int size) {
+  // neural network need non-linear activation
+  // sigmoid func: 1.0/(1.0 + exp(-x))
+for (int i = 0; i < size; i++) {
+    data[i] = 1.0f / (1.0f + expf(-data[i]));
+  }
+}
+
+float dot_product(const float* A, const float* B, int size) {
+  float result = 0;
+  vDSP_dotpr(A, 1, B, 1, &result, size);
+  return result;
 }
 
