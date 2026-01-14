dd2d70b47cbbbecda0b09dcdfb85695a7634e786
Aditya <adityasai3230@gmail.com>
2026-01-14 17:42:48 +0530

    first commit where i defined the config.h file

---
+++ include/config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+#define INPUT_SIZE  784 //28x28 pixels
+#define HIDDEN_SIZE 128
+#define OUTPUT_SIZE 10 // currently i am traning this neural network on digits 0-9
+#define LEARNING_RATE 0.01f
+#define EPOCHS 10
+#endif // !DEBUG
